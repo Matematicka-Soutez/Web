1ecb6ec487cfdec241127084feb3570f05f41d7a
jurajkuliska <juraj.kuliska@strv.com>
2018-10-23 14:48:56 +0200

    Fix vector drawable crashing API19 and adjust style for API19 due to a know bug in support library

--- android/MaSo/app/src/main/java/cz/cuni/mff/maso/tools/adapters.kt
+++ android/MaSo/app/src/main/java/cz/cuni/mff/maso/tools/adapters.kt
@@ -1,10 +1,18 @@
 package cz.cuni.mff.maso.tools
 
 import android.content.res.ColorStateList
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 @BindingAdapter("fabBgColor")
 fun FloatingActionButton.setColor(color: Int) {
 	backgroundTintList = ColorStateList.valueOf(color)
+}
+
+@BindingAdapter("srcImage")
+fun ImageView.setSrcImage(@DrawableRes drawableRes: Int) {
+	setImageDrawable(VectorDrawableCompat.create(context.resources, drawableRes, context.theme))
 }
