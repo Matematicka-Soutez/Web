e40b7178206438848b8131e070c7d65e6a5f2962
Jiri Erhart <jiri.erhart@strv.com>
2019-04-08 09:20:36 +0200

    pm2 config

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,22 @@
 'use strict'
 
+const fs = require('fs')
+
 module.exports = {
   hostname: 'https://maso23.herokuapp.com',
+  database: {
+    options: {
+      operatorsAliases: false,
+      dialectOptions: {
+        ssl: {
+          key: fs.readFileSync('/root/.postgresql/postgresql.key'),
+          cert: fs.readFileSync('/root/.postgresql/postgresql.crt'),
+          ca: fs.readFileSync('/root/.postgresql/root.crt'),
+        },
+      },
+      logging: true,
+    },
+    connectionString: process.env.DATABASE_URL
+      || 'postgresql://maso-web@10.10.11.11/maso?sslmode=verify-full',
+  },
 }
