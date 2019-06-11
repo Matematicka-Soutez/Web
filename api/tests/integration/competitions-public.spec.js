9f3f7cb40b69473eb6fdafe5ed9ae58ce5eeab14
Irina <IrinaQA@Irina-Shirnina.local>
2019-06-11 11:00:57 +0200

    Test for two teams second round

--- api/tests/integration/competitions-public.spec.js
+++ api/tests/integration/competitions-public.spec.js
@@ -419,7 +419,85 @@ describe('Public competition API endpoints: /api/competitions', function competi
           }],
         })
         .expect('Content-Type', /json/u)
-        .expect(409)
+        .expect(409, {
+          type: 'CONFLICT',
+          message: 'V tuto chvíli nemůžete registrovat více než jeden tým z jedné školy.',
+        })
+    })
+    it('FAIL 409 Conflict - should allow only 2 teams per school during 2nd round', async function getTeams() {
+      const school = await createSchool()
+      await db.Competition.update({
+        date: moment().add(7, 'days').toISOString(),
+        registrationRound1: moment().subtract(7, 'days').toISOString(),
+        registrationRound2: moment().subtract(1, 'days').toISOString(),
+        registrationRound3: moment().add(7, 'days').toISOString(),
+        registrationEnd: moment().add(9, 'days').toISOString(),
+      }, { where: { id: 3 } })
+      await request(this.server)
+        .post(`/api/competitions/current/registration/${school.accessCode}`)
+        .send({
+          competitionVenueId: 5,
+          teamName: 'Masožravci',
+          members: [{
+            firstName: 'Jan',
+            lastName: 'Černý',
+            grade: 8,
+          }, {
+            firstName: 'Honza',
+            lastName: 'Bílý',
+            grade: 8,
+          }, {
+            firstName: 'Jitka',
+            lastName: 'Fialová',
+            grade: 8,
+          }],
+        })
+        .expect('Content-Type', /json/u)
+        .expect(201)
+      await request(this.server)
+        .post(`/api/competitions/current/registration/${school.accessCode}`)
+        .send({
+          competitionVenueId: 5,
+          teamName: 'Vegani',
+          members: [{
+            firstName: 'Jan',
+            lastName: 'Černý',
+            grade: 8,
+          }, {
+            firstName: 'Honza',
+            lastName: 'Bílý',
+            grade: 8,
+          }, {
+            firstName: 'Jitka',
+            lastName: 'Fialová',
+            grade: 8,
+          }],
+        })
+        .expect('Content-Type', /json/u)
+        .expect(201)
+      await request(this.server)
+        .post(`/api/competitions/current/registration/${school.accessCode}`)
+        .send({
+          competitionVenueId: 5,
+          teamName: 'Treti',
+          members: [{
+            firstName: 'Jan',
+            lastName: 'Černý',
+            grade: 8,
+          }, {
+            firstName: 'Honza',
+            lastName: 'Bílý',
+            grade: 8,
+          }, {
+            firstName: 'Jitka',
+            lastName: 'Fialová',
+            grade: 8,
+          }],
+        })
+        .expect('Content-Type', /json/u)
+        .expect(409, { 
+          type: 'CONFLICT',
+          message: 'V tuto chvíli nemůžete registrovat více než dva týmy z jedné školy.' })
     })
 
   })
